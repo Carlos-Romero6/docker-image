b8427fb3cc4d7e0ba26ad6ec927aa4971fb20c44
Carlos Romero <carloshernanromerogonzalez@gmail.com>
2025-05-24 11:34:30 -0400

    Leroy desgraciado

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    text = "XD"
+    text = "Leroy es un desgraciado uwu"
     videourl = "static/video.mp4"
     return render_template("index.html", text=text, videourl=videourl)
 
